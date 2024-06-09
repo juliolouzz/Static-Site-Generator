051753b05b154ff2531f300f2f49f79e2fde9858
juliolouzz <jlouz.dev@gmail.com>
2024-06-09 11:38:13 +0100

    add function to split images and links + tests

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -5,6 +5,8 @@ from inline_markdown import (
     split_nodes_delimiter,
     extract_markdown_images,
     extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link,
 )
 
 from textnode import (
@@ -13,6 +15,8 @@ from textnode import (
     text_type_bold,
     text_type_italic,
     text_type_code,
+    text_type_link,
+    text_type_image,
 )
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -138,6 +142,80 @@ class TestMarkdownExtraction(unittest.TestCase):
         ]
         result = extract_markdown_links(text)
         self.assertEqual(result, expected)
+
+class TestSplitNodes(unittest.TestCase):
+
+    def test_split_nodes_image(self):
+        # Test with text containing two images
+        node = TextNode(
+            "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("This is text with an ", text_type_text),
+            TextNode("image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode(" and another ", text_type_text),
+            TextNode("second image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png"),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+        # Test with text containing no images
+        node = TextNode(
+            "This is text with no images.",
+            text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [node]
+        self.assertEqual(new_nodes, expected)
+
+        # Test with text containing an image at the start
+        node = TextNode(
+            "![start](url1) some text",
+            text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("start", text_type_image, "url1"),
+            TextNode(" some text", text_type_text)
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_nodes_link(self):
+        # Test with text containing two links
+        node = TextNode(
+            "This is text with a [link](https://www.example.com) and another [second link](https://www.example.com/another)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("link", text_type_link, "https://www.example.com"),
+            TextNode(" and another ", text_type_text),
+            TextNode("second link", text_type_link, "https://www.example.com/another"),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+        # Test with text containing no links
+        node = TextNode(
+            "This is text with no links.",
+            text_type_text,
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [node]
+        self.assertEqual(new_nodes, expected)
+
+        # Test with text containing a link at the start
+        node = TextNode(
+            "[start](url1) some text",
+            text_type_text,
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("start", text_type_link, "url1"),
+            TextNode(" some text", text_type_text)
+        ]
+        self.assertEqual(new_nodes, expected)
 
 if __name__ == "__main__":
     unittest.main()
